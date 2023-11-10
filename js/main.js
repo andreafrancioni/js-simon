3c9b2d7aaec9c637502de6c03676d3078ce69922
andreafrancioni <andreafrancioni.work@gmail.com>
2023-11-10 01:58:03 +0100

    Create funzioni timer, inserimento numero e di verifica numeri

--- js/main.js
+++ js/main.js
@@ -1,9 +1,49 @@
 
+let numbersElement = document.getElementById("numbers");
 let listaNum = [];
+let userNum = [];
+let points = 0;
+
 
 for(i=0; i<5; i++){
     listaNum[i]=randomNumberGen(100);
 }
+
+numbersElement.innerHTML += listaNum;
+
+setTimeout(hideNumber, 1000)
+setTimeout(guessNumber, 1100)
+
+checkNumber();
+
+function hideNumber(){
+    numbersElement.innerHTML = "";
+}
+
+/*** FUNZIONE PER RIMUOVERE NUMERI IN PAGINA E FAR INSERIRE ALL'UTENTE I NUMERI CHE RICORDA ***/
+function guessNumber(){
+    
+    for(i=0; i<5; i++){
+        userNum[i]=prompt("Inserisci uno alla volta i numeri che ti ricordi.")
+    }
+    
+}
+
+function checkNumber(){
+
+    for(i=0; i<5; i++){
+        if(userNum.includes(listaNum)){
+            points += 1
+        }
+        else{
+            console.log("numero non presente");
+        }
+    }
+
+    console.log(`Ne hai indovinate ${points} su 5`);
+
+}
+
 
 /*** FUNZIONE GENERA NUMERI CASUALI ***/
 function randomNumberGen(max){
